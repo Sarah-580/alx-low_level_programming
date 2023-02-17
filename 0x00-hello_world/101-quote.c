3f533fd5dcb5f2c59568526beb8cb8848ff2cc3b
Sarah <projectcode580@gmail.com>
2023-02-16 18:21:54 -0800

    0-preprocessor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A Cprogram that prints a line to the standard error
+ * main - A C program that prints a line to the standard error
  * return: 1 (Success)
  */
 int main(void)
