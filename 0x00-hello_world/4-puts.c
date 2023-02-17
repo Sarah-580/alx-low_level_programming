538e2e094a7c678ce24fdae8ef240bf0fdd87713
Sarah <projectcode580@gmail.com>
2023-02-16 16:32:16 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C programming entry point
+ * return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - C programming entry point
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
