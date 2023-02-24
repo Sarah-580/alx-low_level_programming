d69a4bf9265186f1be4a9a934b8feeb65651bab3
Sarah <projectcode580@gmail.com>
2023-02-23 17:01:27 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,11 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (x = 0; x <= n; x++)
-		_putchar(' ');
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('_');
+		}
+	}
 	_putchar('\n');
 }
