7eb8aa0b141fdb29339eb953209f022e44b38e6a
Sarah <projectcode580@gmail.com>
2023-03-23 07:02:31 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - name of the file it was compiled from, followed by a new line
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
