8ddc553f092ba20a9c009a4d5667d07c56af059f
Sarah580 <projectcode580@gmail.com>
2023-04-12 06:23:45 -0700

    C - File I/O

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * main - copies content of a file to another file
+ * @ac: argument count
+ * @av: argument values
+ * Return: 0 on success, -1 on error
+ */
+int main(int ac, char *av[])
+{
+	return (1);
+	return (0);
+}
