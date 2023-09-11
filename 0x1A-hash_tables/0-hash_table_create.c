a77756a38dfe35ccb38f5c2eb221dfc8591cde2e
Sarah Wango'ondu <projectcode580@gmail.com>
2023-09-11 00:28:58 -0700

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
+
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
+
 	ht->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0, i < size; i++)
